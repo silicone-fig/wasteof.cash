89bd7ae5ae8c8d03a0a08a5ed5cd59b29ec0f3ed
silicone-fig <me@siliconecb.cc>
2024-10-18 09:02:34 -0400

    update packages, fix tsconfig

--- src/routes/+layout.server.js
+++
@@ -1,7 +0,0 @@
-// thank you for being kool :)
-
-export function load({ setHeaders }) {
-  setHeaders({
-    "X-Made-With": "<3 from siliconecb_",
-  });
-}
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import buildInsrpt from './build-insrpt';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import buildInsrpt from "./build-insrpt";
 
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
     fs: {
-      allow: ['static']
-    }
+      allow: ["static"],
+    },
   },
   build: {
     rollupOptions: {
-      external: ['node-fetch']
-    }
-  }
+      external: ["node-fetch"],
+    },
+  },
+  ssr: {
+    noExternal: ["@melt-ui/svelte", "bits-ui"],
+  },
 });
